ced7fff8b96ea56d2969f4d6fb8f042b13d08950
Haydenj213 <johns3hm@mail.uc.edu>
2025-03-06 15:07:44 -0500

    Update DevTest.java

--- src/DevTest.java
+++ src/DevTest.java
@@ -3,20 +3,7 @@ import java.util.InputMismatchException;
 public class DevTest {
     public static void main(String[] args) {
 
-    }
 
-    public static double CtoF(double Celsius) {
-
-        System.out.printf("%-10s %-10s%n", "Celsius", "Fahrenheit");
-        System.out.println("----------------------");
-
-        for (double celsius = -100.0; Celsius <= 100.0; Celsius++) {
-            double fahrenheit = CtoF(Celsius);
-            System.out.printf("%-10d %-10.2f%n", celsius, fahrenheit);
-        }
-
-            return (Celsius * 9 / 5) + 32;
-        }
     }
 
 
